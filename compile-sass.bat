a42d7328b05e93bafd6e362cf2f10dc08d65ff98
Macklin Hartley <macklin@outlook.com>
2016-07-30 13:54:10 +0800

    Upload data file

---
+++ compile-sass.bat
@@ -0,0 +1,1 @@
+node-sass "css/site.scss" "css/site.css"
--- js/site.js
+++ js/site.js
@@ -2,16 +2,44 @@
 var app = angular.module("streetSharkApp", []);
 
 /* main controller */
-app.controller("MainController", ["$scope",
-    function ($scope) {
-            
-        $scope.defaultUrl = "pages/home.html";
-        $scope.templateUrl = $scope.defaultUrl;
-        
-        $scope.goToPage = function (pageName) {
-            
-            var url = "pages" + pageName + ".html";
-            
-            $scope.templateUrl = url;
-        }
-    }]);
+app.controller("MainController", ["$scope", "heatmap",
+	function ($scope, heatmap) {
+
+		$scope.map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: -31.9307918, lng: 115.8581252},
+			zoom: 17
+		});
+
+		$scope.heatmapLayer = {};
+
+		heatmap.fetch().then(function (data) {
+			mapPoints(data);
+		});
+
+		function mapPoints(data) {
+
+			var points = [];
+			angular.forEach(data, function(value, key) {
+				points.push(new google.maps.LatLng(value.lat, value.long));
+			});
+
+			$scope.heatmapLayer = new google.maps.visualization.HeatmapLayer({
+				data: points,
+				map: $scope.map
+			});
+		}
+	}]);
+
+app.factory('heatmap', function ($timeout, $http) {
+	var heatmap = {
+		fetch: function () {
+			return $timeout(function () {
+				return $http.get('./data/heatmap.json').then(function (response) {
+					return response.data;
+				});
+			}, 30);
+		}
+	}
+
+	return heatmap;
+});
